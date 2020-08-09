644bde938842a476abaa8bfc610aa0de836e4e1f
creci <creci@mail.ru>
2020-08-09 12:24:09 +0300

    Removed gps for aric box. Added the airc-box-dhcp launch argument.

--- src/logger/airc_box_data_processor.c
+++ src/logger/airc_box_data_processor.c
@@ -7,9 +7,8 @@
 #include "airc_box_data_structure.h"
 
 #define AIRC_BOX_PORT (11333)
-#define AIRC_BOX_DHCP "192.168.0.103"
 
-int airc_box_connect(int *airc_box_sock, struct sockaddr_in *airc_box_sockaddr)
+int airc_box_connect(int *airc_box_sock, const char *dhcp, struct sockaddr_in *airc_box_sockaddr)
 {
     if ((*airc_box_sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
     {
@@ -20,7 +19,7 @@ int airc_box_connect(int *airc_box_sock, struct sockaddr_in *airc_box_sockaddr)
     airc_box_sockaddr->sin_family = AF_INET;
     airc_box_sockaddr->sin_port  = htons(AIRC_BOX_PORT);
 
-    if(inet_pton(AF_INET, AIRC_BOX_DHCP, &(airc_box_sockaddr->sin_addr)) <= 0)
+    if(inet_pton(AF_INET, dhcp, &(airc_box_sockaddr->sin_addr)) <= 0)
     {
         printf("airc_box: invalid address\n");
         return -2;
